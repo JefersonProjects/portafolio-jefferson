11f1a57d0b9d6c11ba3865bef03eb348618246ec
Jefferson Calderon <jeffersonalejandro943@gmail.com>
2026-01-03 12:26:38 -0500

    Actualización de Informacion

--- skills.js
+++ skills.js
@@ -11,6 +11,7 @@ const skillsData = [
             { name: "Angular ", icon: "devicon-angular-plain" },
             { name: "React", icon: "devicon-react-original" },
             { name: "AWS", icon: "devicon-amazonwebservices-plain-wordmark" },
+            { name: "NodeJs", icon: "devicon-nodejs-plain" },
             { name: "SQL", icon: "" }
         ]
     },
@@ -19,7 +20,8 @@ const skillsData = [
         items: [
             { name: "SQL Server", icon: "devicon-microsoftsqlserver-plain" },
             { name: "MySQL", icon: "devicon-mysql-plain" },
-            { name: "PostgreSQL", icon: "devicon-postgresql-plain" }
+            { name: "PostgreSQL", icon: "devicon-postgresql-plain" },
+            { name: "Oracle", icon: "devicon-oracle-plain" }
         ]
     },
     {
@@ -28,7 +30,11 @@ const skillsData = [
             { name: "VS Code", icon: "devicon-vscode-plain" },
             { name: "Postman", icon: "devicon-postman-plain" },
             { name: "GitHub", icon: "devicon-github-original" },
-            { name: "IntelliJ", icon: "devicon-intellij-plain" }
+            { name: "IntelliJ", icon: "devicon-intellij-plain" },
+            { name: "Git", icon: "devicon-git-plain" },
+            { name: "NetBeans", icon: "devicon-netbeans-plain" },
+            { name: "Docker", icon: "devicon-docker-plain" }
+
         ]
     },
     {
